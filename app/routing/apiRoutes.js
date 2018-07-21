200c95f3dbefd111ae0c271809541f482857a264
Candice Schramm <candiceschramm@gmail.com>
2018-07-21 17:54:37 -0600

    when the suer clicks the submit button, the data they entered is saved to the API

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,10 +6,6 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(users)
     })
-    app.get("/api/friends/", function(req, res){
-        console.log("this is a request");
-    });
-    
     app.post("/api/friends", function(res,res){
         console.log("this is a post")
     });
